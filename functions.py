387dc81ffb168cdd34e565ca905a4812f400cdec
Charlotte Sarfati <charlotte@charlottes-air.attlocal.net>
2017-05-07 23:48:27 -0700

    WOP pseudocode

---
+++ functions.py
@@ -0,0 +1,13 @@
+def display_introduction():
+
+def print_main_menu():
+
+def execute_repl_in_main_menu():
+
+def check_airway_questions():
+
+def initiate_breathing_questions():
+
+def perform_compression_questions():
+
+def display_conclusion_with_score():
