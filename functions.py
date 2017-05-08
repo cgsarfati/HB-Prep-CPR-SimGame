bfde3ba393b7d5dc1a6c6449051b830165738d09
Charlotte Sarfati <charlotte@charlottes-air.attlocal.net>
2017-05-08 01:55:29 -0700

    roughdraft1: finished creating functions, WOP 5/7 functions, 2/7 not touched

--- functions.py
+++ functions.py
@@ -1,13 +1,39 @@
 def display_introduction():
+    """Prints introductory scenario that user is in"""
 
-def print_main_menu():
+    return "WOP"
+
+def display_main_menu():
+    """Prints main menu and asks user to make a choice"""
+
+    return "WOP"
 
 def execute_repl_in_main_menu():
+    """Executes repl loop depending on what user decided to choose in
+    main menu"""
 
-def check_airway_questions():
+    return "WOP"
 
-def initiate_breathing_questions():
+def questions_check_airway():
+    """Chronologically displays airway questions with user input. Once all
+    questions are completed, user will be brought back to main menu"""
 
-def perform_compression_questions():
+    return "WOP"
+
+def questions_initiate_breathing():
+    """Chronologically displays breathing questions with user input. Once all
+    questions are completed, user will be brought back to main menu"""
+
+    return "WOP"
+
+def questions_perform_compression():
+    """Chronologically displays compression questions with user input. Once all
+    questions are completed, user will be brought back to main menu"""
+
+    return "WOP"
 
 def display_conclusion_with_score():
+    """Prints successful revival scenario of victim and display's user's total
+    score with numerical ranges that indicate how well user did in game"""
+
+    return "WOP"
--- roughdraft1.py
+++ roughdraft1.py
@@ -1,13 +1,199 @@
 def display_introduction():
+    """Prints introductory scenario that user is in"""
 
-def print_main_menu():
+    print "WOP display introduction text"
+
+
+def get_main_menu():
+    """Prints main menu and asks user to make a choice"""
+
+    print '\n   Main menu that asks user to click options below'
+    print '     a. Check airway'
+    print '     b. Initiate breathing'
+    print '     c. Perform compressions'
+    print '     d. Exit game \n'
+
+    user_choice = raw_input("What would you like to do? ")
+
+    return user_choice
+    #user_choice to be used in execute_repl_in_main_menu function
+
 
 def execute_repl_in_main_menu():
+    """Executes repl loop depending on what user decides to choose in
+    main menu"""
+
+    while True:
+
+        #transfers return input from get_main_menu function to use in repl loop
+        choice = get_main_menu()
+
+        if choice == "a":
+            #a. check airway
+
+            #call function to display airway questions
+            questions_check_airway()
+
+        elif choice == "b":
+            #b. initiate breathing
+
+            #call function to display breathing questions
+            questions_initiate_breathing()
+
+        elif choice == "c":
+            #c. perform compressions
+
+            #call function to display compressions questions
+            questions_perform_compressions()
+
+        elif choice == "d":
+            #d. exit game
+
+            #exit game
+            break
+
 
 def questions_check_airway():
+    """Chronologically displays airway questions with user input. Once all
+    questions are completed, user will be brought back to main menu"""
+
+    #score starts at 0, will either +1 or stay at current score depending if
+    #user inputs correct/wrong answer
+
+    airway_score = 0
+
+    #dictionaries within a list below. dictionaries used to organize this
+    #multiple choice quiz. default keys are 'question', 'correct_answer',
+    #'options', and 'try_again' (will be used later as added feature).
+    #values can be customized to allow for flexibilty.
+
+    airway_questions = [
+        {
+            'question': 'is person breathing question 1',
+            'correct_answer': str(4),
+            'options': ['answer1', 'answer2', 'answer3', 'rightanswer4'],
+            'try_again': "customizable try again message"},
+        {
+            'question': 'pulse question 2',
+            'correct_answer': str(2),
+            'options': ['answer1', 'rightanswer2', 'answer3', 'answer4'],
+            'try_again': "customizable try again message2"}
+        ]
+
+    for ask in airway_questions:
+
+        print ask['question'] + "?"
+
+        n = 1
+        for options in ask['options']:
+            print "%d) %s" % (n, options)
+            n = n + 1
+
+        response = raw_input("What is your answer? ")
+
+        if response == ask['correct_answer']:
+            print "Correct"
+            airway_score += 1
+            print "Your current score is " + str(airway_score) + " out of 2"
+        else:
+            print "Sorry, the right answer is " + ask['correct_answer'] + ")"
+            continue
+
 
 def questions_initiate_breathing():
+    """Chronologically displays breathing questions with user input. Once all
+    questions are completed, user will be brought back to main menu"""
 
-def questions_perform_compression():
+    breathing_score = 0
+
+    breathing_questions = [
+        {
+            'question': 'airway technique question 1',
+            'correct_answer': str(4),
+            'options': ['answer1', 'answer2', 'answer3', 'rightanswer4'],
+            'try_again': "customizable try again message"},
+        {
+            'question': 'amount of breathes question 2',
+            'correct_answer': str(2),
+            'options': ['answer1', 'rightanswer2', 'answer3', 'answer4'],
+            'try_again': "customizable try again message2"},
+        {
+            'question': 'amount of breathes question 2',
+            'correct_answer': str(2),
+            'options': ['answer1', 'rightanswer2', 'answer3', 'answer4'],
+            'try_again': "customizable try again message2"}
+        ]
+
+    for ask in breathing_questions:
+
+        print ask['question'] + "?"
+
+        n = 1
+        for options in ask['options']:
+            print "%d) %s" % (n, options)
+            n = n + 1
+
+        response = raw_input("What is your answer? ")
+
+        if response == ask['correct_answer']:
+            print "CORRECT"
+            breathing_score += 1
+            print "Your current score is " + str(breathing_score) + " out of 3"
+        else:
+            print "Sorry, the right answer is " + ask['correct_answer'] + ")"
+            continue
+
+
+def questions_perform_compressions():
+    """Chronologically displays compression questions with user input. Once all
+    questions are completed, user will be brought back to main menu"""
+
+    compressions_score = 0
+
+    compressions_questions = [
+        {
+            'question': 'rate question 1',
+            'correct_answer': str(4),
+            'options': ['answer1', 'answer2', 'answer3', 'rightanswer4'],
+            'try_again': "customizable try again message"},
+        {
+            'question': 'depth question 2',
+            'correct_answer': str(2),
+            'options': ['answer1', 'rightanswer2', 'answer3', 'answer4'],
+            'try_again': "customizable try again message2"},
+        {
+            'question': 'location question 3',
+            'correct_answer': str(2),
+            'options': ['answer1', 'rightanswer2', 'answer3', 'answer4'],
+            'try_again': "customizable try again message2"}
+        ]
+
+    for ask in compressions_questions:
+
+        print ask['question'] + "?"
+
+        n = 1
+        for options in ask['options']:
+            print "%d) %s" % (n, options)
+            n = n + 1
+
+        response = raw_input("What is your answer? ")
+
+        if response == ask['correct_answer']:
+            print "CORRECT"
+            compressions_score += 1
+            print "Your current score is " + str(compressions_score) + " out of 3"
+        else:
+            print "Sorry, the right answer is " + ask['correct_answer'] + ")"
+            continue
+
 
 def display_conclusion_with_score():
+    """Prints successful revival scenario of victim and display's user's total
+    score with numerical ranges that indicate how well user did in game"""
+
+    print "WOP conclusion text"
+
+
+display_introduction()
+execute_repl_in_main_menu()
