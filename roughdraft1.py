2338921521c43631ccdde5ee92e30cf628c8d3ff
Charlotte Sarfati <charlotte@charlottes-air.attlocal.net>
2017-05-08 00:00:09 -0700

    WOP roughdraft 1

---
+++ roughdraft1.py
@@ -0,0 +1,13 @@
+def display_introduction():
+
+def print_main_menu():
+
+def execute_repl_in_main_menu():
+
+def questions_check_airway():
+
+def questions_initiate_breathing():
+
+def questions_perform_compression():
+
+def display_conclusion_with_score():
