ffbcb7fd4a5941b14c9877034c68e894a01d796e
Charlotte Sarfati <charlotte@Charlottes-MacBook-Air.local>
2017-05-10 16:58:58 -0700

    renamed functions to align with current draft

--- functions.py
+++ functions.py
@@ -3,16 +3,19 @@ def display_introduction():
 
     return "WOP"
 
-def display_main_menu():
+
+def get_main_menu():
     """Prints main menu and asks user to make a choice"""
 
     return "WOP"
 
-def execute_repl_in_main_menu():
-    """Executes repl loop depending on what user decided to choose in
+
+def execute_user_interaction_in_main_menu():
+    """Executes loop depending on what user decided to choose in
     main menu"""
 
     return "WOP"
+
 
 def questions_check_airway():
     """Chronologically displays airway questions with user input. Once all
@@ -20,19 +23,22 @@ def questions_check_airway():
 
     return "WOP"
 
+
 def questions_initiate_breathing():
     """Chronologically displays breathing questions with user input. Once all
     questions are completed, user will be brought back to main menu"""
 
     return "WOP"
 
-def questions_perform_compression():
+
+def questions_perform_compressions():
     """Chronologically displays compression questions with user input. Once all
     questions are completed, user will be brought back to main menu"""
 
     return "WOP"
 
-def display_conclusion_with_score():
+
+def display_conclusion_with_final_score():
     """Prints successful revival scenario of victim and display's user's total
     score with numerical ranges that indicate how well user did in game"""
 
